3d1f5a874b226e6f95c9cc928fbf916787dafc1d
bagpiper23 <jbendowsky@gmail.com>
2024-01-08 17:54:57 +0200

    Lecture 04.04 - Repeating Data with *ngFor - move higher up

--- src/app/events/events-list.component.ts
+++ src/app/events/events-list.component.ts
@@ -3,10 +3,10 @@ import { Component } from '@angular/core'
 @Component({
     selector: 'events-list',
     template: `
-    <div>
+    <div *ngFor="let event of events">
         <h1>Upcoming Angular Events</h1>
         <hr/>
-        <event-thumbnail *ngFor="let event of events" [event]="event"></event-thumbnail>
+        <event-thumbnail [event]="event"></event-thumbnail>
     </div>
     `
 })
