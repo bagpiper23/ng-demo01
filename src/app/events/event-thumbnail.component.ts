444262c9c34bb36abad755db4e6c1850366509ad
bagpiper23 <jbendowsky@gmail.com>
2024-01-09 15:03:15 +0200

    Lecture 04.10 - Styling Components with ngStyle - example#4

--- src/app/events/event-thumbnail.component.ts
+++ src/app/events/event-thumbnail.component.ts
@@ -6,7 +6,7 @@ import { Component, Input, Output, EventEmitter } from '@angular/core'
     <div class="well hoverwell thumbnail">
         <h2>{{event?.name}}</h2>
         <div>Date: {{event?.date}}</div>
-        <div [ngStyle]="getStartTimeStyle()" [ngSwitch]="event?.time">
+        <div style="..." [ngStyle]="getStartTimeStyle()" [ngSwitch]="event?.time">
             Time: {{event?.time}} 
             <span *ngSwitchCase="'8:00 am'">(Early Start)</span>
             <span *ngSwitchCase="'10:00 am'">(Late Start)</span>
