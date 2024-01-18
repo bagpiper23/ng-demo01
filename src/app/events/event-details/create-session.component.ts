b97815bb65e2509faf6cd221defce051cd1fbc26
bagpiper23 <jbendowsky@gmail.com>
2024-01-18 17:27:01 +0200

    Lecture 07.12 - Creating Custom Validators - complex version

--- src/app/events/event-details/create-session.component.ts
+++ src/app/events/event-details/create-session.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core'
 import { FormControl, FormGroup, Validators } from '@angular/forms'
-import { ISession } from '../shared'
+import { ISession, restrictedWords } from '../shared'
 
 @Component({
     templateUrl: './create-session.component.html',
@@ -26,7 +26,7 @@ export class CreateSessionComponent implements OnInit {
         this.presenter = new FormControl('', Validators.required)
         this.duration = new FormControl('', Validators.required)
         this.level = new FormControl('', Validators.required)
-        this.abstract = new FormControl('', [Validators.required, Validators.maxLength(400), this.restrictedWords])
+        this.abstract = new FormControl('', [Validators.required, Validators.maxLength(400), restrictedWords(['foo','bar'])])
 
         this.newSessionForm = new FormGroup({
             name: this.name,
@@ -36,11 +36,7 @@ export class CreateSessionComponent implements OnInit {
             abstract: this.abstract,
         })
     }
-    private restrictedWords(control: FormControl): {[key: string]: any} {
-        return control.value.includes('foo')
-            ? {'restrictedWords': 'foo'}
-            : null
-    }
+
     saveSession(formValues) {
         let session:ISession = {
             id: undefined,
