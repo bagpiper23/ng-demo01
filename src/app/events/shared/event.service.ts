80db7aee292f89c708b9b1a5d598a89201ff524b
bagpiper23 <jbendowsky@gmail.com>
2024-02-08 12:32:50 +0200

    Lecture 14.05 - Using POST and PUT

--- src/app/events/shared/event.service.ts
+++ src/app/events/shared/event.service.ts
@@ -1,7 +1,7 @@
 import { Injectable, EventEmitter } from '@angular/core'
 import { Subject, Observable, of } from 'rxjs'
 import { IEvent, ISession } from './event.model'
-import { HttpClient } from '@angular/common/http'
+import { HttpClient, HttpHeaders } from '@angular/common/http'
 import { catchError } from 'rxjs/operators'
 
 @Injectable()
@@ -21,14 +21,9 @@ export class EventService {
     }
 
     saveEvent(event) {
-      event.id = 999
-      event.session = []
-      EVENTS.push(event)
-    }
-
-    updateEvent(event) {
-      let index = EVENTS.findIndex(x => x.id = event.id)
-      EVENTS[index] = event
+      let options = { headers: new HttpHeaders({'Content-Type': 'application/json'})}
+      return this.http.post<IEvent>('/api/events', event, options)
+        .pipe(catchError(this.handleError<IEvent>('saveEvent')))
     }
 
     searchSessions(searchTerm: string) {
